86a5851241162c03c03bcc77a70da450b036850b
Leandrox JMG <lean.macias69@gmail.com>
2026-06-10 11:25:20 -0500

    eliminado dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -5,7 +5,7 @@ import { StatsWidget } from './components/statswidget';
     imports: [StatsWidget],
     template: `
         <div class="grid grid-cols-12 gap-8">
-            <app-stats-widget class="contents" /> 
+            
         </div>
     `
 })
